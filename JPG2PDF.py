e9b89a1e6463d9bd25c844bd6f2e1a279e42c419
Jangsoodlor <celica2548@gmail.com>
2022-03-18 14:28:04 +0700

    add some QoL change and a proper way to exit.
    Do I need description?

--- JPG2PDF.py
+++ JPG2PDF.py
@@ -4,70 +4,79 @@ from PIL import Image
 import os
 
 
+while True:
+    # --------------- USER INPUT -------------------- #
 
-# --------------- USER INPUT -------------------- #
+    folder = str(input("insert your picture's folder directory: "))                                          # Folder containing all the images.
+    name = str(input("insert your desired document name: "))+".pdf"                                          # Name of the output PDF file.
+    moonmai = input("press 1 if you want to auto-rotate picture, press other number if you don't: ")
+    size = input("insert the paper size: ")
 
-folder = str(input("insert your picture's folder directory: "))                                          # Folder containing all the images.
-name = str(input("insert your desired document name: "))+".pdf"                                          # Name of the output PDF file.
-moon = int(input("press 1 if you want to auto-rotate picture, press other number if you don't: "))
-size = input("insert the paper size: ")
+    if moonmai == "":
+        moonmai = 1
 
-# ---------------FPDF--------------------------------#
-pdf = FPDF(format = str(size))
-imagelist = []                                                 # Contains the list of all images to be converted to PDF.
-
-# --------------- DEFINE OUTPUT PAPER SIZE --------------------#
-
-if size == "A4" or size == "a4":
-    x = int(210)
-    y = int(297)
-elif size == "A5" or size == "a5":
-    x = int(148)
-    y = int (210)
-elif size == "A3" or size == "a3":
-    x = int(297)
-    y = int (420)
+    moon = int(moonmai)
+    
+    if size == "":
+        size == str(A4)
 
 
-# ------------- ADD ALL THE IMAGES IN A LIST ------------- #
+    # ---------------FPDF--------------------------------#
+    pdf = FPDF(format = str(size))
+    imagelist = []                                                 # Contains the list of all images to be converted to PDF.
 
-for dirpath, dirnames, filenames in os.walk(folder):
-    for filename in [f for f in filenames]:
-        full_path = os.path.join(dirpath, filename)
-        imagelist.append(full_path)
+    # --------------- DEFINE OUTPUT PAPER SIZE --------------------#
 
-imagelist.sort()                                               # Sort the images by name.
-for i in range(0, len(imagelist)):
-    print(imagelist[i])
+    if size == "A4" or size == "a4":
+        x = int(210)
+        y = int(297)
+    elif size == "A5" or size == "a5":
+        x = int(148)
+        y = int (210)
+    elif size == "A3" or size == "a3":
+        x = int(297)
+        y = int (420)
 
-# --------------- ROTATE ANY LANDSCAPE MODE IMAGE IF PRESENT ----------------- #
-if moon == 1:
+
+    # ------------- ADD ALL THE IMAGES IN A LIST ------------- #
+
+    for dirpath, dirnames, filenames in os.walk(folder):
+        for filename in [f for f in filenames]:
+            full_path = os.path.join(dirpath, filename)
+            imagelist.append(full_path)
+
+    imagelist.sort()                                               # Sort the images by name.
     for i in range(0, len(imagelist)):
-        im1 = Image.open(imagelist[i])                             # Open the image.
-        width, height = im1.size                                   # Get the width and height of that image.
-        if width > height:
-            print("rotating picture")
-            im2 = im1.transpose(Image.ROTATE_270)                  # If width > height, rotate the image.
-            os.remove(imagelist[i])                                # Delete the previous image.
-            im2.save(imagelist[i])                                 # Save the rotated image.
-            # im.save
+        print(imagelist[i])
+
+    # --------------- ROTATE ANY LANDSCAPE MODE IMAGE IF PRESENT ----------------- #
+    if moon == 1:
+        for i in range(0, len(imagelist)):
+            im1 = Image.open(imagelist[i])                             # Open the image.
+            width, height = im1.size                                   # Get the width and height of that image.
+            if width > height:
+                print("rotating picture")
+                im2 = im1.transpose(Image.ROTATE_270)                  # If width > height, rotate the image.
+                os.remove(imagelist[i])                                # Delete the previous image.
+                im2.save(imagelist[i])                                 # Save the rotated image.
+                # im.save
 
 
-print("\nFound " + str(len(imagelist)) + " image files. Converting to PDF....\n")
+    print("\nFound " + str(len(imagelist)) + " image files. Converting to PDF....\n")
 
 
-# -------------- CONVERT TO PDF ------------ #
+    # -------------- CONVERT TO PDF ------------ #
 
-for image in imagelist:
-    pdf.add_page()
-    pdf.image(image, 0, 0, x, y)                           
+    for image in imagelist:
+        pdf.add_page()
+        pdf.image(image, 0, 0, x, y)                           
 
-pdf.output(folder + name, "F")                                 # Save the PDF.
+    pdf.output(folder + name, "F")                                 # Save the PDF.
 
-print("PDF generated successfully!")
+    print("PDF generated successfully!")
+    # ---------------Exit----#
+    print("Exit Mai Yom (y/n)")
+    if input() == "y" or input() == "Y":
+        exit()
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-
-# ------------- EXIT -----------------#
-ork = str(input("press ENTER key to exit..."))
-if ork == "":
-    exit()
